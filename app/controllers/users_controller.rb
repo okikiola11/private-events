863742689b5def74c6f3acf4e1f254212dad563b
okikiola11 <okikiola.apelehin@gmail.com>
2020-09-11 17:54:51 +0000

    Add user sessions Add users controller Add user model Add session controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
   def new
     @user = User.new 
   end
 
   def create
-    @user = User.build(user_params)
+    @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the application!!!"
+      flash[:success] = "Welcome to Private Events!!!"
       redirect_to @user
     else
       render 'new'
     end
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   private
